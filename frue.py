c1380b2ffd91665f927edf063d966d93e3844065
Narek <amirbekiann@gmail.com>
2014-08-14 11:33:39 -0700

    Creating Frue

---
+++ frue.py
@@ -0,0 +1,17 @@
+from random import choice
+
+class Frue:
+    """This class is true, except when it is false"""
+    def __init__(self):
+        pass
+
+    def __bool__(self):
+        return choice([True, False])
+
+    __nonzero__=__bool__
+
+
+if __name__ == '__main__':
+    for i in xrange(100):
+        if Frue():
+            print i
